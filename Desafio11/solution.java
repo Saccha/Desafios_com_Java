f65873184cf0ef401879ea7ee4d45fbb93147a97
Sabrina Haßlocher <62216740+Saccha@users.noreply.github.com>
2021-11-11 19:11:30 -0300

    Create solution.java

---
+++ Desafio11/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HeadOrTail {
+
+    public static void main(final String[] args) {
+        final Scanner input = new Scanner(System.in);
+        while (input.hasNextInt()) {
+            final int cases = input.nextInt();
+
+            if (cases == 0) {
+                break;
+            }
+
+            int mary = 0;
+            int john = 0;
+            for (int i = 0; i < cases; i++) {
+                if (input.nextInt() == 0) {
+                    mary++;
+                } else {
+                    john++;
+                }
+            }
+
+            System.out.printf("Mary won %d times and John won %d times%n", mary, john);
+        }
+    }
+}
